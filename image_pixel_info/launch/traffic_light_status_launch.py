1d682227e858a0bef5733401569eeca5e343ce5a
kugurofu <tenten31569@icloud.com>
2024-11-30 12:31:33 +0000

    edit launch

--- image_pixel_info/launch/traffic_light_status_launch.py
+++ image_pixel_info/launch/traffic_light_status_launch.py
@@ -3,24 +3,16 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        '''
         Node(
             package='image_pixel_info',
-            executable='mjpg_camera_publisher',
-            name='mjpg_camera_publisher',
-            output='screen',
-        ),
-        '''
-        Node(
-            package='image_pixel_info',
-            executable='yolov8',
-            name='yolov8',
+            executable='LMS_image',
+            name='LMS_image',
             output='screen',
         ),
         Node(
             package='image_pixel_info',
-            executable='traffic_light_controller',
-            name='traffic_light_controller',
+            executable='judge_luminance',
+            name='judge_luminance',
             output='screen',
         ),
     ])
